089c1176c381a0c2175e5ba8a5637848ea4d4253
Rachel Parris <rachel.parris@gmail.com>
2018-08-25 23:35:53 -0400

    Able to create comment for a particular thread.

--- server/models/thread-comments.js
+++ server/models/thread-comments.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose');
  
 const comment = new mongoose.Schema({
+  authorId: { 
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: 'Thread' 
+    },
   author: {
     type: String,
     required: true
--- server/routes/comment-routes.js
+++ server/routes/comment-routes.js
@@ -2,23 +2,52 @@ const express = require('express');
 const db = require('../models');
 const router = express.Router();
 
+// threadId = 5b82033d9354034828c942e8
+
 // List all comments per thread
-// router.get('/:thread/comments');
+router.get('/threads/:threadId/comments', (req, res) => {
+  // db.Thread.findOne()
+});
 
 // Create a new comment
-router.post('/:threadId/comments', (req, res) => {
+router.post('/threads/:threadId/comments', (req, res) => {
   const threadId = req.params.threadId;
-  const author = req.params.author;
-  const title = req.params.title;
-  const body = req.body.body;
 
-  
+  // Find the requested thread
+  db.Thread.findOne({ _id: threadId })
+    .then(thread => {
+      console.log(req.body)
+      const author = req.body.author;
+      const title = req.body.title || thread.title;
+      const body = req.body.body;
+
+      // Create comment to be added to thread
+      db.Comment.create({
+        author,
+        title,
+        body,
+        // authorId: id
+      })
+        .then(comment => {
+          thread.comments.push(comment);
+          thread.save();
+          res.status(200).json({ status: 'New comment added.' });
+        })
+        .catch(err => {
+          console.log(err)
+          res.status(400).json({ status: 'Could not add comment.' });
+        });
+    })
+    .catch(err => {
+      // console.log(err);
+      res.status(400).json({ status: 'Cound not find thread by id.' });
+    });
 });
 
 // Update a comment
-// router.put('/:threadId/comments/:commentId');
+// router.put('/threads/:threadId/comments/:commentId');
 
 // Delete a comment
-// router.delete('/:threadId/comments/:commentId');
+// router.delete('/threads/:threadId/comments/:commentId');
 
 module.exports = router;
